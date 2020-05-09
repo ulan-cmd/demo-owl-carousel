38934dd25dbf94b94ac1738864e1f40014139311
ulan-cmd <ulan22.work@gmail.com>
2020-05-09 17:18:14 +0600

    init: demo version

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){  
+
+ $('.owl-carousel').owlCarousel({
+  	items:2,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause:true
+})
+
+});
